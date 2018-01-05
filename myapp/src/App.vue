89d9f6a72bed306f7037a307cfbafa1a26efa1bf
diegominola <drubio@vocento.com>
2018-01-05 12:48:02 +0100

    primera app Vue

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,60 +1,50 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="app" class="app-tareas">
+        <div class="content-app">
+            <!-- Esta es la forma de pasar un parámetro del script al componente titulo -->
+            <titulo v-bind:titulo="titulo"></titulo>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    import Titulo from './TituloComponent.vue'
+    export default {
+        // Esta es la forma de importar los componentes
+        components: {
+            Titulo
+        },
+        data() {
+            return {
+                titulo: "Listado deee tareas",
+            }
+        }
     }
-  }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
+    * {
+        margin: 0;
+        padding: 0;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    body {
+        font-family: Arial, sans-serif;
+    }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+    ul {
+        list-style: none;
+    }
 
-a {
-  color: #42b983;
-}
+    .app-tareas {
+        width: 800px;
+        margin: 0 auto;
+        background-color: #A4A4A4;
+    }
+
+    .content-app {
+        width: 700px;
+        margin: 0 auto;
+    }
 </style>
---
+++ myapp/src/TituloComponent.vue
@@ -0,0 +1,15 @@
+<template id="">
+    <div class='titulo-app'>{{ titulo }}</div>
+</template>
+
+<script>
+    export default {
+        props: ['titulo'],
+        // Data() (ES6) es lo mismo que data: function(){}
+        // data() {
+        //     return {
+        //         titulo: "Listado tareas",
+        //     }
+        // }
+    }
+</script>
